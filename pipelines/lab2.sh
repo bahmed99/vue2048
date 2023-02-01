f079453d4e7642d2d8488c6943927320e955699e
BAHRI Ahmed <ahmed.bahri@bordeaux-inp.fr>
2023-02-01 18:04:46 +0100

    merge

--- pipelines/lab2.sh
+++ pipelines/lab2.sh
@@ -9,3 +9,7 @@ pnpm build
 echo '-------------------------- Construction du "package" à déployer done --------------------------'
 pnpm test
 echo '-------------------------- Exécution des tests done -------------------------- '
+
+outdated_dependencies=$(pnpm outdated --json)
+mkdir -p reports
+echo $outdated_dependencies > reports/outdated_dependencies.json
