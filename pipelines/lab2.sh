089d588f964640b27fa76059fef1d1d150e4a667
CHEIKH Ilyes <icheikh@bordeaux-inp.fr>
2023-02-01 18:00:18 +0100

    update script

--- pipelines/lab2.sh
+++ pipelines/lab2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd .. && pnpm install
-echo "dependecies installed"
+echo "-------------------------- dependecies installed --------------------------"
 pnpm type-check
-echo "Vérification du typage statique du code done"
+echo "-------------------------- Vérification du typage statique du code done --------------------------"
 pnpm lint 
-echo "Analyse statique du code done"
+echo "-------------------------- Analyse statique du code done --------------------------"
 pnpm build 
-echo 'Construction du "package" à déployer done'
+echo '-------------------------- Construction du "package" à déployer done --------------------------'
 pnpm test
-echo 'Exécution des tests done'
+echo '-------------------------- Exécution des tests done -------------------------- '
